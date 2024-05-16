d4faa050be90ba7f2c531348fd34a0450496df8a
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-05-16 17:08:58 +0400

    Add logic for drawing books from an array of book objects

--- scripts/script.js
+++ scripts/script.js
@@ -13,31 +13,52 @@ function addBookToLibrary(title, author, date, pages, format) {
   let newBook = new Book(title, author, date, pages, format);
   myLibrary.push(newBook);
 
-  const newContainer = document.createElement("div");
-  const newImg = document.createElement("img");
-  const newInfoDiv = document.createElement("div");
-  const newTitle = document.createElement("h2")
-  const newAuthorSpan = document.createElement("span");
-  const newDateSpan = document.createElement("span");
-  const newPagesSpan = document.createElement("span");
-
-  newImg.setAttribute("src", "https://m.media-amazon.com/images/I/81C+pXJfbdL._AC_UF1000,1000_QL80_DpWeblab_.jpg");
-
-  newContainer.classList.add("book-container");
-  newAuthorSpan.classList.add("book-author");
-  newDateSpan.classList.add("book-date");
-  newPagesSpan.classList.add("book-pages");
-
-  newTitle.textContent = newBook.title;
-  newAuthorSpan.textContent = newBook.author;
-  newDateSpan.textContent = newBook.date;
-  newPagesSpan.textContent = newBook.pages;
-
-  newInfoDiv.appendChild(newTitle);
-  newInfoDiv.appendChild(newAuthorSpan);
-  newInfoDiv.appendChild(newDateSpan);
-  newInfoDiv.appendChild(newPagesSpan);
-  newContainer.appendChild(newImg);
-  newContainer.appendChild(newInfoDiv);
-  main.appendChild(newContainer);
 }
+
+function drawBooks(library) {
+  for (book of library) {
+    const newContainer = document.createElement("div");
+    const newImg = document.createElement("img");
+    const newInfoDiv = document.createElement("div");
+    const newTitle = document.createElement("h2")
+    const newAuthorSpan = document.createElement("span");
+    const newDateSpan = document.createElement("span");
+    const newPagesSpan = document.createElement("span");
+  
+    newImg.setAttribute("src", "https://m.media-amazon.com/images/I/81C+pXJfbdL._AC_UF1000,1000_QL80_DpWeblab_.jpg");
+  
+    newContainer.classList.add("book-container");
+    newAuthorSpan.classList.add("book-author");
+    newDateSpan.classList.add("book-date");
+    newPagesSpan.classList.add("book-pages");
+  
+    newTitle.textContent = book.title;
+    newAuthorSpan.textContent = book.author;
+    newDateSpan.textContent = book.date;
+    newPagesSpan.textContent = book.pages;
+  
+    newInfoDiv.appendChild(newTitle);
+    newInfoDiv.appendChild(newAuthorSpan);
+    newInfoDiv.appendChild(newDateSpan);
+    newInfoDiv.appendChild(newPagesSpan);
+    newContainer.appendChild(newImg);
+    newContainer.appendChild(newInfoDiv);
+    main.appendChild(newContainer);
+  }
+}
+
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+addBookToLibrary("Metro 2033", "Dmitry Glukhovsky", "18 March 2010", "458 Pages", "Paperback");
+
+
+drawBooks(myLibrary);
